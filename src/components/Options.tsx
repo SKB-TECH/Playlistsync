5cf3403db62608f4914f1481beb06fc39fd85cf0
SKB-TECH <shakokinyamba201@gmail.com>
2024-05-31 14:36:18 +0200

    delete option

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {Checkbox, Editable, EditableInput, EditablePreview} from "@chakra-ui/react";
 import {COLORS} from "@/utils";
 
